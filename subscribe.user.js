2a05844955d685ca9228a1deba9f2587e4d817d2
7twin <32747235+7twin@users.noreply.github.com>
2018-05-25 00:08:48 +0200

    Made subscribe link same color
    The subscribe link now blends in with the other links next to it, both on mouse hover and default state

--- subscribe.user.js
+++ subscribe.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Devrant Subscribe Button
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Add a button to subscribe to rant comments
 // @author       7twin
 // @match        https://devrant.com/rants/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $(".rant-extras:first").html($(".rant-extras:first").html() + '<a href="#" class="rantlist-footer-link" id="subscribe-bttn" style="margin-left:10px;">Subscribe</a>');
+    GM_addStyle("#subscribe-bttn {opacity: 0.7;margin-left:10px;} #subscribe-bttn:hover { opacity: 1; }");
+    $(".rant-extras:first").html($(".rant-extras:first").html() + '<a href="#" class="rantlist-footer-link" id="subscribe-bttn">Subscribe</a>');
     $("#subscribe-bttn").on("click",function(e){
         e.preventDefault();
         $(".comment-text-input:first").val("📌 Subscribing to the comments");
